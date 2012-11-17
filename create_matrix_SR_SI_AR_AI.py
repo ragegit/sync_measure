4029ad7788ef6be9ab1ade90e3f18d586f9abfab
Leo Settele <leo.settele@gmail.com>
2012-11-17 13:22:23 +0100

    all raster functions have the same syntax

--- create_matrix_SR_SI_AR_AI.py
+++ create_matrix_SR_SI_AR_AI.py
@@ -5,6 +5,46 @@ import random as rn
 import matplotlib.pyplot as plt
 import brian as br
 
+# Helper functions	
+def smaller_step(raster):
+	newraster = np.insert(raster,np.arange(0,len(raster[0])),0,axis=1)
+	return newraster
+	
+def add_noise(raster):
+	newraster = np.zeros(np.shape(raster))
+	idx = np.where(raster==1)
+	newidx = [[],[]]
+	i=0
+	for element in idx[1]:
+		x = np.random.randint(3)
+		if(x==2):
+			element=(element+1)%len(raster[0])
+		elif(x==1):
+			element=(element-1)%len(raster[0])
+		newidx[1].append(element)
+		newidx[0].append(idx[0][i])
+		i=i+1
+	i=0
+	for element in newidx[0]:
+		newraster[element][newidx[1][i]] = 1
+		i=i+1
+	return newraster
+
+def mixed(N,Tmax,popspikes):
+	mat = np.floor(1./9*np.random.randint(10, size=(N,Tmax)))
+	number = int((float(Tmax))/popspikes)
+	for j in xrange(N):
+		for i in xrange(Tmax):
+			if(i%number == 0):
+				x = np.random.randint(4)
+				if(x==2 or x==1 or x==0):
+					mat[j][i] = 1
+				else:
+					mat[j][i] = 0
+	return mat
+
+# actual raster funktions
+# signature is func(N,Tmax) -> raster
 def SR(N,Tmax):
 	arr = np.zeros(Tmax, dtype='int')
 	liste = []
@@ -79,30 +119,6 @@ def AI(N,Tmax):
 def Poisson(N,Tmax):
 	return np.floor(1./999*np.random.randint(1000, size=(N,Tmax)))
 	
-def smaller_step(raster):
-	newraster = np.insert(raster,np.arange(0,len(raster[0])),0,axis=1)
-	return newraster
-	
-def add_noise(raster):
-	newraster = np.zeros(np.shape(raster))
-	idx = np.where(raster==1)
-	newidx = [[],[]]
-	i=0
-	for element in idx[1]:
-		x = np.random.randint(3)
-		if(x==2):
-			element=(element+1)%len(raster[0])
-		elif(x==1):
-			element=(element-1)%len(raster[0])
-		newidx[1].append(element)
-		newidx[0].append(idx[0][i])
-		i=i+1
-	i=0
-	for element in newidx[0]:
-		newraster[element][newidx[1][i]] = 1
-		i=i+1
-	return newraster
-	
 def mixed5(N,Tmax):
 	return mixed(N,Tmax,5)
 def mixed10(N,Tmax):
@@ -110,15 +126,9 @@ def mixed10(N,Tmax):
 def mixed20(N,Tmax):
 	return mixed(N,Tmax,20)
 
-def mixed(N,Tmax,popspikes):
-	mat = np.floor(1./9*np.random.randint(10, size=(N,Tmax)))
-	number = int((float(Tmax))/popspikes)
-	for j in xrange(N):
-		for i in xrange(Tmax):
-			if(i%number == 0):
-				x = np.random.randint(4)
-				if(x==2 or x==1 or x==0):
-					mat[j][i] = 1
-				else:
-					mat[j][i] = 0
-	return mat
+def smaller_step_SR(N, Tmax):
+	return smaller_step(SR(N,Tmax))
+def smaller_step_noisy_SR(N, Tmax):
+	return add_noise(smaller_step(SR(N,Tmax)))
+def smaller_step_more_noisy_SR(N, Tmax):
+	return add_noise(add_noise(smaller_step(SR(N,Tmax))))
